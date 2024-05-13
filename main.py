4e33a3fea5b6fad952d0b8127aa74743aef8fe7c
Алексей Коган <alekseikogan@yandex.ru>
2024-05-13 23:12:30 +0300

    валидация на выдачу и запись данных

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from typing import List
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
 app = FastAPI(
     title='Trading App'
@@ -18,20 +20,47 @@ fake_trades = [
 ]
 
 
-@app.get('/users/{user_id}')
+class Trade(BaseModel):
+    """Класс Сделка"""
+    id: int
+    user_id: int
+    currency: str = Field(max_length=5)
+    side: str
+    price: float = Field(ge=0)
+    amount: float = Field(g=0)
+
+
+class User(BaseModel):
+    """Класс Пользователь"""
+    id: int
+    role: str
+    name: str
+
+
+@app.get('/users/{user_id}', response_model=List[User])
 def get_user(user_id: int):
+    """Получение данных пользователя по id"""
     return [user for user in fake_users if user.get('id') == user_id]
 
 
 @app.get('/trades')
 def get_trades(limit: int = 2, offset: int = 0):
+    """Получение истории трейдов"""
     return fake_trades[offset:][:limit]
 
 
 @app.post('/users/{user_id}')
 def change_user_name(user_id: int, new_name: str):
+    """Изменение имени пользователя по id"""
     current_user = list(filter(lambda user: user.get('id') == user_id, fake_users))
     if current_user:
         current_user[0]['name'] = new_name
         return {'status': 200, 'data': f'Имя пользователя успешно изменено на {new_name}'}
     return {'status': 404, 'data': f'Пользователь с id={user_id} не найден.'}
+
+
+@app.post('/trades')
+def add_trades(trade: List[Trade]):
+    """Добавление данных сделки"""
+    fake_trades.extend(trade)
+    return {'status': 200, 'data': trade}
