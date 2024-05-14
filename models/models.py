81c7c15e553438a7dfe2f259c897b1582c6d8bba
Алексей Коган (рабочий) <alekseikogan@yandex.ru>
2024-05-14 15:50:48 +0300

    #4 Созданы и применены миграции

---
+++ models/models.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from sqlalchemy import (JSON, TIMESTAMP, Column, ForeignKey, Integer, MetaData,
+                        String, Table)
+
+metadata = MetaData()
+
+roles = Table(
+    'roles',
+    metadata,
+    Column('id', Integer, primary_key=True),
+    Column('name', String, nullable=False),
+    Column('permissions', JSON),
+    )
+
+users = Table(
+    'users',
+    metadata,
+    Column('id', Integer, primary_key=True),
+    Column('email', String, nullable=False),
+    Column('username', String, nullable=False),
+    Column('password', String, nullable=False),
+    Column('registered_ad', TIMESTAMP, default=datetime.utcnow),
+    Column('role_id', Integer, ForeignKey('roles.id'))
+)
